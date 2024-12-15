9b22e67269a36a6ad615e0933d73182720ae5ed7
Saif Hossain Jibon <saifhossainjibon@gmail.com>
2024-12-15 11:54:28 +0600

    basic of c

--- data_type_and_variables.c
+++ data_type_and_variables.c
@@ -1,10 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n;
-    // integer numbers declare
-    // integer numbers declare
-    scanf("%d",&n);
-    printf("%d",n);
+int main()
+{
+    // integer numbers declare (%d)
+    int a;
+    char ch;
+    float b;
+    double c;
+    scanf("%d", &a);
+    getchar();
+    scanf("%c", &ch);
+    scanf("%f %lf", &b, &c);
+    printf("%05d %c %.2f %.5lf", a, ch, b, c);
+
+    // char  (%d)
+    // Short int  (%d)
+    // int %d
+    // long int %ld
+    // long long int %lld
+    // char %c
+    // float %f
+    // double %Lf
+    // long double int %Lf
+
+    // scanf("%d",&n);
+    // printf("%d",n+1);
     return 0;
 }
